2ffe603abc0c1bda1dc77a9a8ec2661ab6818bbe
Kenny-R <18-10595@usb.ve>
2023-10-09 18:23:40 -0400

    mejora en la solucion con hilos

--- taller 1/opcion_no_tan_facil/main.c
+++ taller 1/opcion_no_tan_facil/main.c
@@ -12,6 +12,11 @@ enum acciones
     AGREGAR = 4,
 };
 
+// variables de contenido de las jarras
+int contenidoJarraChica = 0;
+int contenidoJarraGrande = 0; 
+
+
 // con estas variables controlaremos las acciones que hara cada jarra
 // en cada turno
 enum acciones accion = ESPERA;
@@ -58,49 +63,41 @@ void jarraGrande()
 
     // Primer paso
     llenar(&contenido, max);
-    printf("\tLlenaste la jarra grande\n");
+    printf("\tJarra Grande: Llenaste la jarra grande\n");
 
     // Segundo paso
     mover(&contenido, 3);
+    printf("\tJarra Grande: Moviste 3 lt de la jarra grande a la pequeña\n");
     semaforoPropio = 1;
-    printf("\tMoviste 3 lt de la jarra grande a la pequeña\n");
     esperar(0);
 
     // Tercer paso
     accion = VACIAR;
     semaforoPropio = 1;
-    printf("\tVaciaste la jarra pequeña\n");
     esperar(0);
 
     // Cuarto paso
     mover(&contenido, 2);
+    printf("\tJarra Grande: Moviste 2 lt a la jarra pequeña\n");
     semaforoPropio = 1;
-    printf("\tMoviste 2 lt de la jarra grande a la pequeña\n");
     esperar(0);
 
     // Quinto paso
     llenar(&contenido, 5);
-    printf("\tLlenaste la jarra grande\n");
+    printf("\tJarra Grande: Llenaste la jarra grande\n");
 
     // Sexto paso
     mover(&contenido, 1);
+    printf("\tJarra Grande: Moviste 1 lt de la jarra grande a la pequeña\n");
     semaforoPropio = 1;
-    printf("\tMoviste 1 lt de la jarra grande a la pequeña\n");
     esperar(0);
 
-    if (contenido == 4)
-    {
-        printf("Felicidades desactivaste la bomba!\n");
-    }
-    else
-    {
-        printf("Muy mal, acabas de morir\n");
-    }
+    contenidoJarraGrande = contenido;
     accion = FIN;
     semaforoPropio = 1;
 }
 
-void jarraChica()
+void jarraChica(int *controladorContenido)
 {
     int contenido = 0;
     int max = 3;
@@ -118,10 +115,12 @@ void jarraChica()
         else if (accion == VACIAR)
         {
             vaciar(&contenido);
+            printf("\tJarra pequeña: Vaciaste la jarra pequeña\n");
             semaforoPropio = 0;
         }
         else if (accion == FIN)
         {
+            contenidoJarraChica = contenido;
             break;
         }
     }
@@ -139,6 +138,16 @@ int main(int argc, char const *argv[])
     // Esperamos a que los hilos terminen
     pthread_join(h1, NULL);
     pthread_join(h2, NULL);
+
+
+    if (contenidoJarraGrande == 4 && contenidoJarraChica == 3)
+    {
+        printf("Felicidades desactivaste la bomba!\n");
+    }
+    else
+    {
+        printf("Muy mal, acabas de morir\n");
+    }
 
     return 0;
 }
